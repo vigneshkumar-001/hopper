6398db7ecc404331c619330deb8b09eff2ea5fc9
vigneshkumar-001 <vignesh@fenizotechnologies.com>
2026-06-08 10:26:40 +0530

    ios build updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import Firebase
 import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
 
   override func application(
       _ application: UIApplication,
